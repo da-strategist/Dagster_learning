62d5f134c548ca0048ffab0917d05126000c0238
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2025-08-18 02:40:47 +0300

    data ingestion into duckdb

--- src/ETL_project/test_connection.py
+++ src/ETL_project/test_connection.py
@@ -10,6 +10,20 @@ import os
 #testing connection to duckdb
 
 
-conn = duckdb.connect(database=':memory:')
-print("connection successful")
-conn.close()
+#conn = duckdb.connect(database=':memory:')
+#print("connection successful")
+#conn.close()
+
+
+#import duckdb
+
+# connect to your db file
+conn = duckdb.connect("src/ETL_project/data/raw_duckdb.duckdb")
+
+# list all tables
+print(conn.execute("SHOW TABLES").fetchall())
+
+# peek at the raw_orders table
+print(conn.execute("SELECT * FROM main.raw_orders LIMIT 5").fetchdf())
+print(conn.execute("select * from main.raw_payments LIMIT 5").fetchdf())
+print(conn.execute("select * from main.raw_customer_data LIMIT 5").fetchdf())
